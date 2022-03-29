a9ad9028a720d5192fbfb277215247208b369006
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-03-29 23:15:16 +0900

    navigation 설정 및 click 설정

--- presentation/src/main/java/com/example/presentation/view/MainFragment.kt
+++ presentation/src/main/java/com/example/presentation/view/MainFragment.kt
@@ -1,5 +1,7 @@
 package com.example.presentation.view
 
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.example.presentation.R
 import com.example.presentation.base.BaseFragment
 import com.example.presentation.databinding.FragmentMainBinding
@@ -8,6 +10,10 @@ import com.example.presentation.databinding.FragmentMainBinding
 class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
     override fun init() {
 
+    }
+
+    fun clickBtn(view : View) {
+        this.findNavController().navigate(R.id.action_mainFragment_to_manNameFragment)
     }
 
 }
